d4912a5db4c36fc30b2d59b4281291b792bff771
Shvoter <67557033+Shvoter@users.noreply.github.com>
2022-05-11 20:07:09 +0300

    Added  TASK1 solution

---
+++ src/Task1/Main.java
@@ -0,0 +1,17 @@
+package Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Person Artem = new Person();
+        Person Denis = new Person();
+        Person Alica = new Person();
+        Person Carl = new Person();
+        Person Anton = new Person();
+
+        Artem.input("Artem", 2002);
+        Denis.input("Denis", 2007);
+        Alica.input("Alica", 2000);
+        Carl.input("Carl", 2004);
+        Anton.input("Anton", 2010);
+    }
+}
---
+++ src/Task1/Person.java
@@ -0,0 +1,51 @@
+package Task1;
+
+import java.time.Year;
+
+public class Person {
+    private String name;
+    private int birthYear;
+
+    public Person() {
+        name = "unknown";
+        birthYear = Year.now().getValue();
+    }
+
+    public Person(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int age() {
+        return Year.now().getValue() - birthYear;
+    }
+
+    public  void input(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public void output() {
+        System.out.printf("Person {\nname: %s,\nyear of birth: %d,\n}", name, birthYear);
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+}
